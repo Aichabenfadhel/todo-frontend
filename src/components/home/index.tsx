505bc6343eec0d560122a21d38d5bab497d76715
aicha <aichaaichabff@gmail.com>
2023-07-10 15:17:00 +0000

    feat : create `Home` component

---
+++ src/components/home/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./style.css"
+
+export default function Home() {
+  return (
+    <div className="homeContainer">
+      <div className="titleContainer">
+        <h1> Welcome to Todo List</h1>
+        <h2>You can Organize your day and write all the necessary tasks</h2>
+        <Link to="/todo">
+          <button type="button" className="btn btn-primary">
+            Get Started
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
