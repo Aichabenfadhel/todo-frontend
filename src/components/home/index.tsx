890026feeff2e261d5469e7a66181eaf1e4221d8
aicha <aichaaichabff@gmail.com>
2023-07-14 13:03:51 +0000

    fix : fix `home ` component

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -10,15 +10,18 @@ export default function Home() {
     <>
     <NavbarComponent />
     <div className="homeContainer">
+      <div className="welcomeContainer">
       <div className="titleContainer">
         <h1> Welcome to Todo List</h1>
-        <h2>You can Organize your day and write all the necessary tasks</h2>
+        <h2>You can Organize your day and manage all your necessary tasks</h2>
         <Link to="/Login">
           <button type="button" className="btn btn-primary">
             Get Started
           </button>
         </Link>
       </div>
+      <div className="HomeimageContainer"></div>
+    </div>
     </div>
     </>
   );
