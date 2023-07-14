cd2631527076a0293da1232c4362c44658e11711
aicha <aichaaichabff@gmail.com>
2023-07-14 10:03:45 +0000

    feat : add `UserNavBar` component in ``TodoIndex`

--- src/components/Todoindex/index.tsx
+++ src/components/Todoindex/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+
 import InputTodo from "../Input";
 import ListTodos from "../ListTodo";
+import UserNavbarComponent from "../Navbar-cont/userNavbar";
+import { todoIndexTypeProps } from "../types";
 
-export default function TodoIndex(){
-    return(
-        <React.Fragment>
-            <InputTodo />
-            <ListTodos />
-        </React.Fragment>
-    )
+
+
+export default function TodoIndex(
+   {email,password}:todoIndexTypeProps
+) {
+  return (
+    <React.Fragment>
+      <UserNavbarComponent />
+      <InputTodo  email={email} password={password} />
+      <ListTodos email={email} password={password} />
+    </React.Fragment>
+  );
 }
