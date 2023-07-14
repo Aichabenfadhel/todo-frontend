71ba8b65e32a0a76a4d06e8e6fdbb0211425fc88
aicha <aichaaichabff@gmail.com>
2023-07-14 10:12:55 +0000

    fix: fixing `ListTodos` component in `ListTodos`

--- src/components/ListTodo/index.tsx
+++ src/components/ListTodo/index.tsx
@@ -1,46 +1,40 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-
 import { Button, List } from "reactstrap";
 import { BsFillCheckCircleFill, BsFillTrashFill } from "react-icons/bs";
 import { BsListTask } from "react-icons/bs";
-
 import EditTodo from "../EditTodo";
-
+import { ListTodoType, TodoType, listTodoPropsType } from "../types";
 import "./Style.css";
 
-export type ListTodoType = {
-  todo_id: number;
-  description: string;
-  completed: boolean;
-};
+export default function ListTodos({ email, password }: listTodoPropsType) {
+  const [emailinitial, setEmail] = useState(email);
+  const [passwordinitial, setPassword] = useState(password);
+  const [todos, setTodos] = useState<Record<string, TodoType>>();
+  const [todosArray, setTodosArray] = useState<ListTodoType[]>([]);
 
-export default function ListTodos() {
-  const updatedTodos: any = [];
-  const [todos, setTodos] = React.useState<ListTodoType[]>([]);
+  useEffect(() => {
+    getTodos();
+  }, [todosArray]);
 
-  //completed tasks
-
-  const markDone = (todo_id: number, completed: boolean) => {
+  const markDone = (todo_id: string, completed: boolean) => {
     try {
       axios
         .put(
-          `${process.env.REACT_APP_API_URL}/updateTodoCompleted/${todo_id}`,
+          `${process.env.REACT_APP_API_URL}/updateTodocompleted/${email}/${password}/${todo_id}`,
           {
             completed: !completed,
           }
         )
-        .then((): void => {
-          let newTasks = todos.map((todo: any) => {
+        .then(() => {
+          const newTasks = todosArray.map((todo) => {
             if (todo.todo_id === todo_id) {
               return { ...todo, completed: !todo.completed };
             }
             return todo;
           });
-          setTodos(newTasks);
+          setTodosArray(newTasks);
         })
-
         .catch((error: any) => {
           console.error(error.message);
         });
@@ -49,14 +43,14 @@ export default function ListTodos() {
     }
   };
 
-  //delete todo function
-
-  const deleteTodo = async (todo_id: number) => {
+  const deleteTodo = async (todo_id: string) => {
     try {
       axios
-        .delete(`${process.env.REACT_APP_API_URL}/deleteTodo/${todo_id}`)
-        .then((): void => {
-          setTodos(todos.filter((todo: any) => todo.todo_id !== todo_id));
+        .delete(
+          `${process.env.REACT_APP_API_URL}/deleteTodo/${email}/${password}/${todo_id}`
+        )
+        .then(() => {
+          setTodosArray(todosArray.filter((todo) => todo.todo_id !== todo_id));
         })
         .catch((error) => {
           console.error(error.message);
@@ -65,13 +59,13 @@ export default function ListTodos() {
       console.error(err.message);
     }
   };
-  //get todos
+
   const getTodos = async () => {
     try {
       axios
-        .get(`${process.env.REACT_APP_API_URL}/allTodos`)
-        .then((response: any): void => {
-          setTodos(response.data.data);
+        .get(`${process.env.REACT_APP_API_URL}/allTodos/${email}/${password}`)
+        .then((response: any) => {
+          setTodos(response.data.data.todo);
         })
         .catch((error) => {
           console.error(error.message);
@@ -81,9 +75,20 @@ export default function ListTodos() {
     }
   };
 
-  React.useEffect(() => {
-    getTodos();
-  }, [updatedTodos]);
+  useEffect(() => {
+    if (todos !== undefined) {
+      const todosUserArray: ListTodoType[] = [];
+      for (const [id, todo] of Object.entries(todos)) {
+        const todoUser = {
+          todo_id: id,
+          description: todo!.description,
+          completed: todo!.completed,
+        };
+        todosUserArray.push(todoUser);
+      }
+      setTodosArray(todosUserArray);
+    }
+  }, [todos]);
 
   return (
     <div className="list-container">
@@ -92,86 +97,49 @@ export default function ListTodos() {
       </h1>
       <div className="box">
         <List className="list-todos">
-          {updatedTodos.length > 0
-            ? updatedTodos.map((todo: any, index: number) => (
-                <li key={todo.todo_id}>
-                  <div className="todo-container">
-                    <div className={todo.completed ? "done" : ""}>
-                      <span className="id-container"> {index + 1} </span>
-                      <span> {todo.description} </span>
-                    </div>
+          {todosArray.map((todo, index) => {
+            return (
+              <li key={todo.todo_id}>
+                <div className="todo-container">
+                  <div className={todo.completed ? "done" : ""}>
+                    <span className="id-container"> {index + 1} </span>
+                    <span> {todo.description} </span>
                   </div>
+                </div>
 
-                  <div className="editors">
-                    <span title="Task Completed / Not Completed">
-                      <Button
-                        className="button"
-                        onClick={(e) => markDone(todo.todo_id, todo.completed)}
-                      >
-                        <BsFillCheckCircleFill className="icon-editors" />
-                      </Button>
+                <div className="editors">
+                  <span title="Task Completed / Not Completed">
+                    <Button
+                      className="button"
+                      onClick={() => markDone(todo.todo_id, todo.completed)}
+                    >
+                      <BsFillCheckCircleFill className="icon-editors" />
+                    </Button>
+                  </span>
+                  {!todo.completed && (
+                    <span title="Edit">
+                      <EditTodo
+                        email={email}
+                        password={password}
+                        todos={todos}
+                        todo_id={todo.todo_id}
+                        description={todo.description}
+                        updatedTodos={todosArray}
+                      />
                     </span>
-                    {todo.completed ? null : (
-                      <span title="Edit">
-                        <EditTodo
-                          todos={todos}
-                          todo_id={todo.todo_id}
-                          description={todo.description}
-                          updatedTodos={updatedTodos}
-                        />
-                      </span>
-                    )}
-
-                    <span title="Delete">
-                      <Button
-                        className="button"
-                        onClick={() => deleteTodo(todo.todo_id)}
-                      >
-                        <BsFillTrashFill className="icon-editors" />
-                      </Button>
-                    </span>
-                  </div>
-                </li>
-              ))
-            : todos.map((todo: any, index: number) => (
-                <li key={todo.todo_id}>
-                  <div className="todo-container">
-                    <div className={todo.completed ? "done" : ""}>
-                      <span className="id-container"> {index + 1} </span>
-                      <span> {todo.description} </span>
-                    </div>
-                  </div>
-
-                  <div className="editors">
-                    <span title="Task Completed / Not Completed">
-                      <Button
-                        className="button"
-                        onClick={(e) => markDone(todo.todo_id, todo.completed)}
-                      >
-                        <BsFillCheckCircleFill className="icon-editors" />
-                      </Button>
-                    </span>
-                    {todo.completed ? null : (
-                      <span title="Edit">
-                        <EditTodo
-                          todos={todos}
-                          todo_id={todo.todo_id}
-                          description={todo.description}
-                          updatedTodos={updatedTodos}
-                        />
-                      </span>
-                    )}
-                    <span title="Delete">
-                      <Button
-                        className="button"
-                        onClick={() => deleteTodo(todo.todo_id)}
-                      >
-                        <BsFillTrashFill className="icon-editors" />
-                      </Button>
-                    </span>
-                  </div>
-                </li>
-              ))}
+                  )}
+                  <span title="Delete">
+                    <Button
+                      className="button"
+                      onClick={() => deleteTodo(todo.todo_id)}
+                    >
+                      <BsFillTrashFill className="icon-editors" />
+                    </Button>
+                  </span>
+                </div>
+              </li>
+            );
+          })}
         </List>
       </div>
     </div>
